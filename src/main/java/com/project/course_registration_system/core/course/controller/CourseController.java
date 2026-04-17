37266a15720853ab362282edf95d2c8ebc4ac7f8
ahj30420 <ahj30420@naver.com>
2026-04-17 17:09:44 +0900

    feat: 강의 상태 변경 api 추가

--- src/main/java/com/project/course_registration_system/core/course/controller/CourseController.java
+++ src/main/java/com/project/course_registration_system/core/course/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.project.course_registration_system.core.course.controller;
 import com.project.course_registration_system.common.response.ApiResponse;
 import com.project.course_registration_system.common.util.UrlCreator;
 import com.project.course_registration_system.core.course.domain.CourseStatus;
+import com.project.course_registration_system.core.course.dto.UpdateCourseStatusRequest;
 import com.project.course_registration_system.core.course.dto.response.CourseResponse;
 import com.project.course_registration_system.core.course.dto.request.CreateCourseRequest;
 import com.project.course_registration_system.core.course.dto.response.CourseSummaryResponse;
@@ -14,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -55,4 +58,16 @@ public class CourseController implements CourseSpec {
         return ResponseEntity.ok(response);
     }
 
+    @Override
+    @PatchMapping("/{courseId}/status")
+    public ResponseEntity<ApiResponse<CourseResponse>> changeStatus(
+            @PathVariable Long courseId,
+            @Valid @RequestBody UpdateCourseStatusRequest request
+    ) {
+        CourseResponse data = courseService.changeStatus(courseId, request.status());
+        ApiResponse<CourseResponse> response = ApiResponse.<CourseResponse>builder()
+                .body(data)
+                .build();
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/project/course_registration_system/core/course/controller/CourseSpec.java
+++ src/main/java/com/project/course_registration_system/core/course/controller/CourseSpec.java
@@ -2,14 +2,17 @@ package com.project.course_registration_system.core.course.controller;
 
 import com.project.course_registration_system.common.response.ApiResponse;
 import com.project.course_registration_system.core.course.domain.CourseStatus;
+import com.project.course_registration_system.core.course.dto.UpdateCourseStatusRequest;
 import com.project.course_registration_system.core.course.dto.response.CourseResponse;
 import com.project.course_registration_system.core.course.dto.request.CreateCourseRequest;
 import com.project.course_registration_system.core.course.dto.response.CourseSummaryResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,5 +49,24 @@ public interface CourseSpec {
                     required = false
             )
             @RequestParam CourseStatus status
+    );
+
+
+    @Operation(
+            summary = "강의 상태 변경",
+            description = "강의 상태를 변경합니다."
+    )
+    ResponseEntity<ApiResponse<CourseResponse>> changeStatus(
+            @Parameter(
+                    description = "강의 ID",
+                    required = true
+            )
+            @PathVariable Long courseId,
+
+            @Parameter(
+                    description = "변경된 강의 상태",
+                    required = true
+            )
+            @Valid @RequestBody UpdateCourseStatusRequest request
     );
 }
