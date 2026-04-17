0c20d3d99d4c7d2dfaf953bc8442229c4e6a23ea
ahj30420 <ahj30420@naver.com>
2026-04-17 17:12:09 +0900

    test: CourseService 강의 상태 변경 로직 테스트

--- src/test/java/com/project/course_registration_system/core/course/service/CourseServiceTest.java
+++ src/test/java/com/project/course_registration_system/core/course/service/CourseServiceTest.java
@@ -1,17 +1,21 @@
 package com.project.course_registration_system.core.course.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.project.course_registration_system.common.exception.BaseException;
+import com.project.course_registration_system.common.exception.code.CourseErrorCode;
 import com.project.course_registration_system.core.course.domain.Course;
 import com.project.course_registration_system.core.course.domain.CourseStatus;
 import com.project.course_registration_system.core.course.dto.request.CreateCourseRequest;
 import com.project.course_registration_system.core.course.dto.response.CourseResponse;
 import com.project.course_registration_system.core.course.repository.CourseRepository;
 import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -69,4 +73,46 @@ class CourseServiceTest {
         verify(courseRepository, times(1)).save(any(Course.class));
     }
 
+    @Test
+    @DisplayName("강의 상태 변경 테스트: 성공")
+    void change_status_success() throws Exception {
+        // given
+        Long courseId = 1L;
+        CourseStatus newStatus = CourseStatus.OPEN;
+
+        Course course = Course.builder()
+                .title("테스트")
+                .description("테스트 입니다.")
+                .price(1000L)
+                .capacity(10)
+                .creatorId(1L)
+                .startDate(LocalDate.of(2026, 4, 16))
+                .endDate(LocalDate.of(2026, 4, 21))
+                .status(CourseStatus.DRAFT)
+                .build();
+
+        when(courseRepository.findById(courseId)).thenReturn(Optional.of(course));
+
+        // when
+        CourseResponse response = sut.changeStatus(courseId, newStatus);
+
+        // then
+        assertThat(response.status()).isEqualTo(newStatus);
+        assertThat(course.getStatus()).isEqualTo(newStatus);
+    }
+
+    @Test
+    @DisplayName("강의 상태 변경 테스트: 실패[강의가 존재하지 않는 경우]")
+    void change_status_fail_not_found() throws Exception {
+        // given
+        Long courseId = -1L;
+
+        when(courseRepository.findById(courseId)).thenReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() ->
+                sut.changeStatus(courseId, CourseStatus.OPEN))
+                .isInstanceOf(BaseException.class)
+                .hasMessage(CourseErrorCode.COURSE_NOT_FOUND.getMessage());
+    }
 }
