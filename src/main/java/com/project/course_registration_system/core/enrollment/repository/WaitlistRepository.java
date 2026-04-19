7ff536beb06896f031643f3c179f8ab7ac4f0b6c
ahj30420 <ahj30420@naver.com>
2026-04-19 14:47:47 +0900

    feat: 대기열 순번 조회 쿼리 추가

--- src/main/java/com/project/course_registration_system/core/enrollment/repository/WaitlistRepository.java
+++ src/main/java/com/project/course_registration_system/core/enrollment/repository/WaitlistRepository.java
@@ -1,6 +1,7 @@
 package com.project.course_registration_system.core.enrollment.repository;
 
 import com.project.course_registration_system.core.enrollment.domain.Waitlist;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,4 +18,6 @@ public interface WaitlistRepository extends JpaRepository<Waitlist, Long> {
             countQuery = "select count(w) from Waitlist w where w.userId = :userId"
     )
     Page<Waitlist> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+
+    long countByCourseIdAndCreatedAtBefore(Long cousreId, LocalDateTime createdAt);
 }
