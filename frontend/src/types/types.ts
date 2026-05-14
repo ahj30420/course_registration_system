0573f312f70bf056b70bbf9d97e7e1536bd9cf07
ahj30420 <ahj30420@naver.com>
2026-05-15 00:56:14 +0900

    feat: 공통 타입 정의 추가

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,1 +1,4 @@
 export type Mode = 'student' | 'creator';
+export type Tab = 'courses' | 'my-enrollments' | 'creator';
+
+export type ShowMessage = (type: 'success' | 'error', text: string) => void;
