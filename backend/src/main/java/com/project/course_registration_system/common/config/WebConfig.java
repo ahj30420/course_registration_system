55d29042bcfc1e071ef939f0d1384de4ba06a27c
ahj30420 <ahj30420@naver.com>
2026-05-14 23:40:03 +0900

    feat: cors 설정 추가

---
+++ backend/src/main/java/com/project/course_registration_system/common/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.project.course_registration_system.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
+                .allowedOrigins("http://localhost:3000") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+                .allowedMethods("OPTIONS", "PATCH", "GET","POST","PUT","DELETE") // cors를 허용할 method
+                .allowCredentials(true);
+    }
+}
